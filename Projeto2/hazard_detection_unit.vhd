a99d7984ec05f60d79af64e9c010dc2275289c34
Hugo Mendes <hugospm@al.insper.edu.br>
2018-11-08 17:32:11 -0200

    very beginning of hazard implemention

---
+++ Projeto2/hazard_detection_unit.vhd
@@ -0,0 +1,20 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+ 
+entity hazard_detection_unit is
+	 Port ( 
+
+	 );
+	 
+	 
+end alu;
+ 
+architecture arch of hazard_detection_unit is
+
+	
+begin
+	process(all) begin
+		
+	end process;
+end architecture;
